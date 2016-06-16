d287057421dc984ae32b450fd4e350550c743d45
vhellem <vhellem@gmail.com>
2016-06-16 12:29:50 +0200

    -Added support for React
    removed Controllers for easier access

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,7 +1,10 @@
 package hello;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
 
 @SpringBootApplication
 public class Application {
