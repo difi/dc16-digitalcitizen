4c7452d1560fa49dec1731448928f3b0cbe2ade8
Kumiii <vigdihan@stud.ntnu.no>
2016-06-16 14:57:10 +0200

    Button does not work

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,6 +1,14 @@
+var Paragraph = React.createClass({
+    render: function(){
+        return <div class="paragraph"><h1>Søk sykehjemsplass</h1><button type="button" >Trykk her</button></div>;
+    }
+});
 
+/**return <h1>Søk om sykehjemsplass, {this.props.name}</h1>;**/
+/** <p> Dersom det er deg du</p>**/
 
 ReactDOM.render(
-    <h1>Written by react</h1>,
+    /**<h1>Søk om sykehjemsplass</h1>,**/
+    <Paragraph name = "Olga"/>,
     document.getElementById('content')
-); 
+);
