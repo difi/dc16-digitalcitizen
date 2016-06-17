481ee280da68e43acfd21212969bf2f9147e3e04
vhellem <vhellem@gmail.com>
2016-06-17 09:43:11 +0200

    -Added textfields Component

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,6 +1,44 @@
+var Form = React.createClass({
+
+    render: function() {
+        return (
+            <div>
+
+                {this.props.data.map(function(field, i){
+            return (<Textfield key={i} text={field.name}/>);
+                })}
+            </div>
+        );
+    }
+});
 
 
+
+var Textfield = React.createClass( {
+    getInitialState: function() {
+        return {value: this.props.text};
+    },
+    handleChange: function(event) {
+        this.setState({value: event.target.value});
+    },
+    render: function() {
+
+        return (
+            <input
+                type="text"
+                value={this.state.value}
+                onChange={this.handleChange}
+            />
+        );
+    }
+});
+var data = [
+    { name: "Fødselnr"},
+    {name: "Fornavn"},
+    {  name: "Etternavn"},
+{   name: "Sivilstatus"},
+{   name: "Nasjonalitet"},
+{name: "E-post"}];
 ReactDOM.render(
-    <h1>Written by react</h1>,
-    document.getElementById('content')
-); 
+    <Form data={data}/>, document.getElementById("content")
+);
