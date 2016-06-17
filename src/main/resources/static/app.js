9b3ad0a35fa7a0e54126fded52327490ba46afed
chrihand <christina.h.andresen@gmail.com>
2016-06-17 09:43:03 +0200

    Merge remote-tracking branch 'refs/remotes/origin/master' into expo_c
    # Conflicts:
    #	src/main/resources/static/app.js
    #	src/main/resources/static/index.html

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -2,37 +2,118 @@
 
 ReactDOM.render(
     <h1>Søk sykehjemsplass</h1>,
-    document.getElementById('content')
+    document.getElementById('title')
 
 );
+
+class SearchForMe extends React.Component {
+    render () {
+        return (
+            <div {...this.props}>
+                <input type="text" name="name"/>
+            </div>
+        );
+    }
+}
+
+/** class SearchForOther extends  React.Component {
+    render () {
+        return (
+            const Others = () => {
+                const otherList = ['Personnummer', 'Fornavn', 'Etternavn'];
+                const others = otherList.map((other) => {
+                    return <label> { other }<input type="text" name={other}/></label>
+                });
+                return <ul> { others } </ul>
+            }
+        );
+    }
+} */
+
+class RadioButtonMe extends React.Component {
+    render () {
+        return (
+            <div className="choose-Who">
+                <form action="">
+                    <input type="radio" name="who" value="Me" {...this.props}/>Meg selv
+                    <input type="radio" name="who" value="Other"/>Noen andre
+                </form>
+            </div>
+        );
+    }
+}
+
+class App extends React.Component {
+    constructor () {
+        super();
+
+        this.state = {
+             clicked: false
+        };
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick() {
+        this.setState({
+            clicked: true
+        });
+    }
+
+    render () {
+        return (
+            <div>
+                <RadioButtonMe onClick={this.handleClick} />
+                {this.state.clicked ? <SearchForMe /> : null}
+            </div>
+        );
+    }
+};
+
+/**
+var MegSelv = React.createClass({
+    render: function () {
+        return (<div className="me-Input">
+            <h2>Søk sykehjemsplass for meg selv:</h2>
+            <input type="text" name="name"/>
+        </div>);
+    }
+})
 
 var Buttons = React.createClass({
     getInitialState: function() {
         return {isPressed: false};
     },
 
+
+
     _onClick: function () {
-       /** this.setState({
+       this.setState({
             isPressed: true,
         });
         return (<div className="me-Input">
                 <h2>Søk sykehjemsplass for meg selv:</h2>
                 <input type="text" name="name"/>
-            </div>);*/
-        console.log("Trykkt");
+            </div>);
+        return (
+                <div className="Me">
+                    <MegSelv />
+                </div>
+            );
+
     },
 
         render: function() {
         return ( <div className="choose-Who">
             <form action="">
-                <input type="radio" name="who" value="Me" onClick={this._onClick}/> Meg selv
-                    <input type="radio" name="who" value="Other"/> Noen andre
-                </form>
+                <input type="radio" name="who" value="Me" onClick={this._onClick}/>Meg selv
+                <input type="radio" name="who" value="Other"/>Noen andre
+            </form>
         </div>
             );
     }
 });
-/**
+
 var ButtonMe = React.createClass({
     render: function() {
         return (<div className="me-Button">
@@ -53,14 +134,7 @@ var ButtonOther = React.createClass({
     }
 })
 
-*/var MegSelv = React.createClass({
-    render: function () {
-        return (<div className="me-Input">
-            <h2>Søk sykehjemsplass for meg selv:</h2>
-            <input type="text" name="name"/>
-        </div>);
-    }
-}) 
+*/
 
-ReactDOM.render(<Buttons/>, document.getElementById('test'))
+ReactDOM.render(<App/>, document.getElementById('test'))
 
