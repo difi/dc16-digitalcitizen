21adf77e8d092c41c1110463f8cdc7ff1a0b5665
vhellem <vhellem@gmail.com>
2016-06-23 12:17:55 +0200

    -Submission to backend

---
+++ src/main/java/digitalcitizen/Submission.java
@@ -0,0 +1,13 @@
+package digitalcitizen;
+
+/**
+ * Created by camp-vhe on 23.06.2016.
+ */
+public class Submission {
+    private String age;
+    private String name;
+    public Submission(String age, String name){
+        this.age = age;
+        this.name = name;
+    }
+}
--- src/main/java/digitalcitizen/SubmitController.java
+++ src/main/java/digitalcitizen/SubmitController.java
@@ -6,19 +6,20 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMethod;
+
 @Controller
 public class SubmitController {
 
     @RequestMapping(value = "/send", method = RequestMethod.POST)
     public @ResponseBody String processAJAXRequest(
-            @RequestParam("name") String firstname,
-            @RequestParam("age") String lastname	) {
+            @RequestBody Submission submission ) {
         String response = "";
         // Process the request
         // Prepare the response string
-        System.out.println("FOrm submitted");
-        return ""; 
+        System.out.println("Form submitted");
+        return "";
     }
 
 
