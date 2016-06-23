6eb8dfb496f38560d580771cece422d5a09280f5
vhellem <vhellem@gmail.com>
2016-06-23 11:43:56 +0200

    -Started adding submit to server

--- src/main/frontend/app/Application.jsx
+++ src/main/frontend/app/Application.jsx
@@ -26,8 +26,43 @@ export class Application extends React.Component{
         })
     }
     handleSubmit(){
-        console.log("Submitting");
+        var formdata = {
+            name: "Name",
+            age: "Old"
+        };
+        // Send the form data.
+        var xmlhttp = new XMLHttpRequest();
+        var _this = this;
+        xmlhttp.onreadystatechange = function() {
+            if (xmlhttp.readyState === 4) {
+                var response = JSON.parse(xmlhttp.responseText);
+                if (xmlhttp.status === 200 && response.status === 'OK') {
+                    _this.setState({ type: 'success', message: 'We have received your message and will get in touch shortly. Thanks!' });
+                }
+                else {
+                    _this.setState({ type: 'danger', message: 'Sorry, there has been an error. Please try again later or send us an email at info@example.com.' });
+                }
+            }
+        };
+        xmlhttp.open('POST', 'send', true);
+        xmlhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+        xmlhttp.send(this.requestBuildQueryString(formData));
     }
+    /**
+     * Transforms an object into a URL querystring.
+     *
+     * @param object params
+     * @return string the formatted querystring.
+     */
+    requestBuildQueryString(params) {
+    var queryString = [];
+    for(var property in params)
+        if (params.hasOwnProperty(property)) {
+            queryString.push(encodeURIComponent(property) + '=' + encodeURIComponent(params[property]));
+        }
+    return queryString.join('&');
+}
+
     render() {
         var writesOthers = this.state.writesForOthers;
         var firstRender = this.state.firstRender;
