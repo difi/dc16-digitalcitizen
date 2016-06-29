01c8f4ce9a0db52a00eec3fbd0f1543a99d6144d
Kumiii <vigdihan@stud.ntnu.no>
2016-06-29 08:47:03 +0200

    Added tests to find RadioGroup, check value for radio-buttons has null as initial state, find the two buttons - back and next

--- src/main/frontend/test/NeedsForm-test.js
+++ src/main/frontend/test/NeedsForm-test.js
@@ -13,15 +13,38 @@ import {expect} from 'chai';
 import NeedsForm from '../app/NeedsForm.jsx';
 
 describe("NeedsForm", function() {
-    it('should have two radiobuttons', function () {
-        
+    //Radio-buttons
+    it('should have RadioGroup', function () {
+        const  wrapper = shallow(<NeedsForm/>);
+        expect(wrapper.find('.needs')).to.have.length(1);
     });
 
-    //Sjekk at radio-knappene eksisterer
-    //Kan kun velge 1 knapp av gangen
-    //Sjekk at knappene gjør det de skal
+    it('value for the radio-buttons should have null as initial state', function () {
+        const wrapper = mount(<NeedsForm/>);
+        expect(wrapper.state().value).to.equal(null);
+    });
 
-    //Sjekk at forover og bakoverknappen eksisterer
-    //Sjekk at de gjør det de skal
+    //TODO: Check that the radio-buttons are doing what they are supposed to, when this functionality is added
+    //Look at Simulations on http://brewhouse.io/2016/03/18/accelerate-your-react-testing-with-enzyme.html
+    //for example of how to do this.
+
+    //Next and previous buttons
+    it('should have two "normal" buttons - next and previous', function () {
+        const wrapper = shallow(<NeedsForm/>);
+        expect(wrapper.find('Button')).to.have.length(2);
+    });
+
+    //TODO: Sjekk at de gjør det de skal
+    /*it('back button should go to the previous page when pushed', function () {
+        const wrapper = shallow(<NeedsForm/>);
+        const backbutton = wrapper.find('.button-next');
+        wrapper.setState({adress: null});
+        backbutton.simulate('click');
+        expect(this.props.previousStep).to.equal('1');
+    });
+*/
+
+    //Cannot test if there exists two radio-buttons, nor what happens when one of them are pushed, because
+    //they are "react-radio-group" elements.
 
 });
