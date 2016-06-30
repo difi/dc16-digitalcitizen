680daa13101cd18b8fc1729bfddfb834167db404
Kumiii <vigdihan@stud.ntnu.no>
2016-06-30 13:34:43 +0200

    Tried to test next and previous buttons, but could not make it work. In the comments are some of the code I tried. But, in the documentation from Enzyme they say that they have yet to implement event propagation. To simulate a push to a button in NeedsForm to then call a function in Application we would need event propagation.

--- src/main/frontend/test/Application-test.js
+++ src/main/frontend/test/Application-test.js
@@ -8,15 +8,37 @@ import React from 'react';
 //Mount gives full DOM rendering.
 //Render renders react components to static HTML and analyze the resulting HTML structure.
 
+//import {sinon} from 'sinon';
 import { shallow, mount, render } from 'enzyme';
 import {expect} from 'chai';
 //Import the file we want to test.
 import Application from '../app/Application.jsx';
+import NeedsForm from '../app/NeedsForm.jsx'
 
 describe("Application", function() {
 
     //TODO: test functionality of next and back buttons.
-   /* it('when next page is pushed, render next page', function () {
+
+   /* it('when state = 1, applyingForSelf = false and we run "nextStep" we get to step 2', function () {
+        const wrapper = shallow(<Application/>);
+        wrapper.setState({state: 1});
+        //wrapper.nextStep();
+
+    });*/
+
+
+   /*it('when "next page" button is pushed, render next page', function () {
+       //const handleClickStub = sinon.spy();
+       const wrapper = mount(<Application/>);
+
+
+
+       /*
+       const wrapper = mount(<Application><NeedsForm/></Application>);
+       expect(wrapper.find('Button#next')).to.have.length(1);
+       //backButtonInNeeds.simulate('click');
+
+
         //Render the NeedsForm with fieldValues it is dependent on from Application - so we do not
         // have to also render Application
         //const wrapper = shallow(<NeedsForm fieldValues = {fieldValues} saveValues = {saveValues} nextStep = {nextStep}/>);
@@ -29,7 +51,7 @@ describe("Application", function() {
 
     });
 
-
+/*
     it('back button should go to the previous page when pushed', function () {
         //const wrapper = shallow(<NeedsForm fieldValues = {fieldValues}/>);
         //const backbutton = wrapper.find('#back');
