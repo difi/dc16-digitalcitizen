b09820e7ce37f5ce77d469dece82bcf0cab274b1
Sondre <sondrehjetland@gmail.com>
2016-07-04 12:41:43 +0200

    Updated test data

--- src/main/java/digitalcitizen/utilities/TestData.java
+++ src/main/java/digitalcitizen/utilities/TestData.java
@@ -21,13 +21,13 @@ public class TestData {
     };
 
     public static final Person[] PERSONS = {
-            new Person("01019011111", "TestPerson1", new Address("trondheim", "NO", "trondheim", "testveien 1", "7030"), "99999990"),
-            new Person("01019011112", "TestPerson2", new Address("trondheim", "NO", "trondheim", "testveien 2", "7030"), "99999991"),
-            new Person("01019011113", "TestPerson3", new Address("trondheim", "NO", "trondheim", "testveien 3", "7030"), "99999992"),
-            new Person("01019011114", "TestPerson4", new Address("oslo", "NO", "oslo", "testveien 4", "0001"), "99999993"),
-            new Person("01019011115", "TestPerson5", new Address("oslo", "NO", "oslo", "testveien 5", "0001"), "99999994"),
-            new Person("01019011116", "TestPerson6", new Address("sogndal", "NO", "sogndal", "testveien 6", "6856"), "99999995"),
-            new Person("01019011117", "TestPerson7", new Address("sogndal", "NO", "sogndal", "testveien 7", "6856"), "99999996"),
+            new Person("01019011111", "TestPerson1", new Address("Trondheim", "NO", "Trondheim", "testveien 1", "7030"), "99999990"),
+            new Person("01019011112", "TestPerson2", new Address("Trondheim", "NO", "Trondheim", "testveien 2", "7030"), "99999991"),
+            new Person("01019011113", "TestPerson3", new Address("Trondheim", "NO", "Trondheim", "testveien 3", "7030"), "99999992"),
+            new Person("01019011114", "TestPerson4", new Address("Oslo", "NO", "Oslo", "testveien 4", "0001"), "99999993"),
+            new Person("01019011115", "TestPerson5", new Address("Oslo", "NO", "Oslo", "testveien 5", "0001"), "99999994"),
+            new Person("01019011116", "TestPerson6", new Address("Sogndal", "NO", "Sogndal", "testveien 6", "6856"), "99999995"),
+            new Person("01019011117", "TestPerson7", new Address("Sogndal", "NO", "Sogndal", "testveien 7", "6856"), "99999996"),
     };
 
 }
