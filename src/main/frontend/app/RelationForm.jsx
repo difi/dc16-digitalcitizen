4d8faf326b39a728ba573180de01195a008ed82b
hannasollie <hanna.storaker@gmail.com>
2016-07-08 13:30:43 +0200

    fixes placeholder when switching radiobutton in RelationForm. closes #137

--- src/main/frontend/app/RelationForm.jsx
+++ src/main/frontend/app/RelationForm.jsx
@@ -18,6 +18,9 @@ export class RelationFormClass extends React.Component {
         super(props);
         this.handleClickBack = this.handleClickBack.bind(this);
         this.handleClickNext = this.handleClickNext.bind(this);
+        this.handleGuardianRadioButton = this.handleGuardianRadioButton.bind(this);
+        this.handleFamilyRadioButton = this.handleFamilyRadioButton.bind(this);
+        this.handleOtherRadioButton = this.handleOtherRadioButton.bind(this);
     }
 
     handleClickBack() {
@@ -37,6 +40,21 @@ export class RelationFormClass extends React.Component {
 
             this.props.nextStep(3);
         }
+    }
+
+    handleGuardianRadioButton() {
+        this.props.fields.otherRelation.onChange(null);
+        this.props.fields.typeOfRelation.onChange(null);
+    }
+
+    handleFamilyRadioButton() {
+        this.props.fields.nameOfChild.onChange(null);
+        this.props.fields.otherRelation.onChange(null);
+    }
+
+    handleOtherRadioButton() {
+        this.props.fields.nameOfChild.onChange(null);
+        this.props.fields.typeOfRelation.onChange(null);
     }
 
     saveFieldValues() {
@@ -85,6 +103,19 @@ export class RelationFormClass extends React.Component {
 
         this.props.saveValues(data);
         console.log(data);
+    }
+
+    handleRadioButtonChange(relation){
+
+        switch(relation){
+            case "guardian":
+                this.props.fields.typeOfRelation.onChange(null);
+                break;
+
+
+
+        }
+     
     }
 
     render() {
@@ -180,11 +211,11 @@ export class RelationFormClass extends React.Component {
                 <label className="form-header">Hva er din relasjon til den som søker?</label>
                 <div className="form-container">
                     <form className="relation">
-                        <input type="radio" id="guardian-radio" className="radio-Relation" name="radio-buttons" {...relation} value="guardian" checked={relation.value=="guardian"} />Jeg er verge for den jeg søker på vegne av
+                        <input type="radio" id="guardian-radio" className="radio-Relation" name="radio-buttons" {...relation} value="guardian" checked={relation.value=="guardian"} onClick={this.handleGuardianRadioButton} />Jeg er verge for den jeg søker på vegne av
                         <br/>
-                        <input type="radio" id="family-radio" name="radio-buttons" {...relation} value="family"  checked={relation.value=="family"} />Jeg er i familie med den jeg søker på vegne av
+                        <input type="radio" id="family-radio" name="radio-buttons" {...relation} value="family"  checked={relation.value=="family"} onClick={this.handleFamilyRadioButton}/>Jeg er i familie med den jeg søker på vegne av
                         <br/>
-                        <input type="radio" id="other-radio" name="radio-buttons" className="radio-other" {...relation} value="other"  checked={relation.value=="other"}/>Annet
+                        <input type="radio" id="other-radio" name="radio-buttons" className="radio-other" {...relation} value="other"  checked={relation.value=="other"} onClick={this.handleOtherRadioButton}/>Annet
                     </form>
 
                     {content}
