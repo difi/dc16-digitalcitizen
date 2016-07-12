ad6d9409d6720e2d6ea1effa78e8ba971c5ba4a3
hannasollie <hanna.storaker@gmail.com>
2016-07-12 08:39:01 +0200

    Added textfield and fixed autofill to dropdown in DependentForm. #189
    If user chooses "annet" from the dropdown, then a textfield appears.

--- src/main/frontend/app/DependentForm.jsx
+++ src/main/frontend/app/DependentForm.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Component, PropTypes } from 'react'
+import {Component, PropTypes} from 'react'
 import {reduxForm} from 'redux-form';
 import {getValues} from 'redux-form';
 
@@ -15,7 +15,7 @@ var Checkbox = require('react-bootstrap/lib/Checkbox');
 var Button = require('react-bootstrap/lib/Button');
 var FormControl = require('react-bootstrap/lib/FormControl');
 
-export const fields = ["firstName", "lastName", "phone", "mail", "relation"];
+export const fields = ["firstName", "lastName", "phone", "mail", "relation", "depOtherRelation"];
 
 class DependentForm extends React.Component {
     constructor(props) {
@@ -24,9 +24,6 @@ class DependentForm extends React.Component {
     }
 
     handleClickRemove() {
-        this.setState({
-            showForm: false
-        });
         this.props.onClick();
     }
 
@@ -37,8 +34,7 @@ class DependentForm extends React.Component {
             </Button> : '';
 
 
-
-        const {firstName, lastName, phone, mail, relation}= this.props;
+        const {firstName, lastName, phone, mail, relation, depOtherRelation}= this.props;
         return (
             <div>
                 {deleteButton}
@@ -49,7 +45,8 @@ class DependentForm extends React.Component {
                             <label className="fName">Fornavn</label>
                         </Col>
                         <Col sm={8} md={8}>
-                            <FormControl className="fName" ref="firstName" type="text" placeholder="Fornavn" disabled={this.props.autoFilled} {...firstName}/>
+                            <FormControl className="fName" ref="firstName" type="text" placeholder="Fornavn"
+                                         disabled={this.props.autoFilled} {...firstName}/>
                         </Col>
                     </Row>
                     <Row className="form-row">
@@ -57,7 +54,8 @@ class DependentForm extends React.Component {
                             <label className="eName">Etternavn</label>
                         </Col>
                         <Col sm={8} md={8}>
-                            <FormControl className="eName" ref="lastName" type="text" placeholder="Etternavn" disabled={this.props.autoFilled} {...lastName}/>
+                            <FormControl className="eName" ref="lastName" type="text" placeholder="Etternavn"
+                                         disabled={this.props.autoFilled} {...lastName}/>
                         </Col>
                     </Row>
                     <Row className="form-row">
@@ -65,7 +63,8 @@ class DependentForm extends React.Component {
                             <label className="tlf">Telefon</label>
                         </Col>
                         <Col sm={8} md={8}>
-                            <FormControl className="tlf" ref="phone" type="text" placeholder="Telefonnr" disabled={this.props.autoFilled} {...phone}/>
+                            <FormControl className="tlf" ref="phone" type="text" placeholder="Telefonnr"
+                                         disabled={this.props.autoFilled} {...phone}/>
                             {phone.touched && phone.error && <div>{phone.error}</div>}
                         </Col>
                     </Row>
@@ -74,7 +73,8 @@ class DependentForm extends React.Component {
                             <label className="mail">E-post</label>
                         </Col>
                         <Col sm={8} md={8}>
-                            <FormControl className="mail" ref="mail" type="text" placeholder="E-post" disabled={this.props.autoFilled} {...mail}/>
+                            <FormControl className="mail" ref="mail" type="text" placeholder="E-post"
+                                         disabled={this.props.autoFilled} {...mail}/>
                             {mail.touched && mail.error && <div>{mail.error}</div>}
                         </Col>
                     </Row>
@@ -83,11 +83,27 @@ class DependentForm extends React.Component {
                             <label className="depRel">Relasjon</label>
                         </Col>
                         <Col sm={8} md={8}>
-                            <DropdownList className="depRel" ref='relation' options={dropdownContent.RELATIONS} id="test"
+                            <DropdownList className="depRel" ref='relation' options={dropdownContent.DEPENDENTRELATIONS}
+                                          id="test"
                                           valueField="value" labelField="relation" {...relation}
                                           onChange={change => relation.onChange(change.newValue)}/>
                         </Col>
                     </Row>
+                    {this.props.relation.value == 'Annet' ?
+                        <Row className="form-row">
+                            <Col sm={4} md={4}>
+                            </Col>
+                            <Col sm={8} md={8}>
+                                <FormControl
+                                    type="text"
+                                    ref="depOtherRel"
+                                    className="dep-other-rel"
+                                    pattern="[A-Za-zæøåÆØÅ]"
+                                    placeholder="Skriv inn deres relasjon"
+                                    {...depOtherRelation}
+                                />
+                            </Col>
+                        </Row> : ''}
                 </div>
             </div>
         );
--- src/main/frontend/app/RelationForm.jsx
+++ src/main/frontend/app/RelationForm.jsx
@@ -175,7 +175,7 @@ export class RelationFormClass extends React.Component {
                                 <DropdownList id="1"
                                               ref="familyRelation"
                                               className="family-rel"
-                                              options={dropdownContent.RELATIONS}
+                                              options={dropdownContent.FAMILYRELATIONS}
                                               labelField="relation"
                                               valueField="value"
                                     {...typeOfRelation}
