979f7442dca27285b4f6d62ac9c6a21998c18e4b
Kumiii <vigdihan@stud.ntnu.no>
2016-07-13 13:29:59 +0200

    Fixed testfail

--- src/main/frontend/karma.conf.js
+++ src/main/frontend/karma.conf.js
@@ -7,12 +7,6 @@ module.exports = function(config) {
         frameworks: ['jasmine'], //use jasmine framework to test
         files: [ //List of files/patterns to load in the browser.
             'test/*.js',
-            //'app/*.js',
-            //'app/app.js',
-            //'app/Application.jsx',
-            //'app/static_data/*.js',
-            //'app/FormPages/*.jsx',
-            //'app/FormPages/**/*.jsx'
         ],
 
         preprocessors: {
@@ -26,36 +20,23 @@ module.exports = function(config) {
             noInfo: true
         },
 
-        plugins: [
-            require("karma-webpack"), 'karma-jasmine',
-            'karma-chrome-launcher',
-            require("karma-coverage"),
-            require("karma-sourcemap-loader"),
-            require("karma-phantomjs-launcher")
-        ],
-
         babelPreprocessor: {
             options: {
                 presets: ['react']
             }
         },
-
-        /*phantomjsLauncher: {
-            // Have phantomjs exit if a ResourceError is encountered (useful if karma exits without killing phantom)
-            exitOnResourceError: true
-        },*/
-
         reporters: ['coverage', 'progress' ], //Progress will show the number of tests executed, skipped and total.
         port: 9876, // The port where the web server will be listening.
         colors: true, //Enable colors in the output (reporters and logs).
         logLevel: config.LOG_INFO, //This is the default loglevel. Other possible values: config.LOG_DISABLE, config.LOG_ERROR, config.LOG_WARN, config.LOG_INFO, config.LOG_DEBUG
         autoWatch: true,  //Enable watching files and executing the tests whenever one of these files changes.
-        browsers: ['PhantomJS'], // ['Chrome'], // A list of browsers to launch and capture. When Karma starts up, it will also start up each browser which is placed within this setting.
+        browsers: ['Chrome'], // A list of browsers to launch and capture. When Karma starts up, it will also start up each browser which is placed within this setting.
         singleRun: false,
         coverageReporter: {
             type: 'html',
             dir: 'coverage/',
-            file : 'coverage.txt'
+            file : 'coverage.txt',
+
         }
     })
 };
