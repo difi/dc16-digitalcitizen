02ec8865c5088f4965c2e2fee65d7aece5ea7115
vhellem <vhellem@gmail.com>
2016-07-21 09:26:04 +0200

    Revert "Updated proxy config"

--- src/main/frontend/app/FormPages/RelationForm.js
+++ src/main/frontend/app/FormPages/RelationForm.js
@@ -88,11 +88,10 @@ export class RelationFormClass extends React.Component {
     saveFieldValues() {
         // Get values via this.refs
         const {fields: {relation, typeOfRelation, otherRelation, nameOfChild, isDependent}} = this.props;
-        console.log(nameOfChild.value);
-        var pnr = nameOfChild.value.split(":")[0];
+
+
         if (relation.value == "guardian") {
             var dataDep = {
-                pnr: pnr,
                 relation: relation.value,
                 dependent: true,
                 applyingForSelf: false,
@@ -100,7 +99,7 @@ export class RelationFormClass extends React.Component {
 
             this.props.saveValues(dataDep);
             $.ajax({
-                url: RESTpaths.PATHS.MUNICIPALITY_BASE + '?pnr=' + pnr,
+                url: RESTpaths.PATHS.MUNICIPALITY_BASE + '?pnr=' + this.props.userData.pnr,
                 dataType: 'text',
                 cache: false,
                 success: function (data) {
